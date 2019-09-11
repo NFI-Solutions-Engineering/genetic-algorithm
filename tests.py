e8af688afb195485937423240d82062ccf7f4766
christopherpryer <christophpryer@gmail.com>
2019-09-11 19:09:53 +0100

    loaded testing data

--- tests.py
+++ tests.py
@@ -3,11 +3,17 @@ from genetic_algorithm import algorithms as algs
 import unittest
 import pandas as pd
 import numpy as np
+from os import path
+
+root_dir = path.dirname(path.abspath(__name__))
+instance_dir = path.join(root_dir, 'instance')
 
 class BasicVRPGACase(unittest.TestCase):
     """Use case for solving VRP using a genetic algorithm"""
-    initial_route_ids = np.array([]) # each index maps to same position in demand
-    demand_data = pd.DataFrame() # rows for demand to route
+    filepath = path.join(instance_dir, 'demand.csv')
+    demand_data = pd.read_csv(filepath) # rows for demand to route
+    initial_route_ids = demand_data.index.values # each index maps to same position in demand
+
     def setUp(self):
         self.routes = env.Individual(self.initial_route_ids, self.demand_data)
         self.alg = algs.BasicGeneticAlgorithm(
